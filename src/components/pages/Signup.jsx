c9efbe439629b632fbe05295b78b77e7bb36b27d
Shruti <shrutigawande1910@gmail.com>
2023-12-16 23:42:36 +0530

    chore: Updated Signup Page

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -10,12 +10,14 @@ export default function Signup() {
   const [signUpFormData, setSignUpFormData] = useState({
     name: "",
     email: "",
+    phone: "",
     password: "",
   });
+
   const [showNotify, setShowNotify] = useState(false);
   const [notifyData, setNotifyData] = useState({
-    type: '',
-    message: '',
+    type: "",
+    message: "",
   });
 
   const setFormData = (e) => {
@@ -25,16 +27,33 @@ export default function Signup() {
   const registerUser = async (e) => {
     try {
       e.preventDefault();
-      console.log(signUpFormData);
-      const data = await apiConnection(apiEndpoints.REGISTER_USER_ENDPOINT, httpMethods.POST, signUpFormData);
-      console.log(data);
-      if (data.status === 201) {
+      const data = await apiConnection(
+        apiEndpoints.REGISTER_USER_ENDPOINT,
+        httpMethods.POST,
+        signUpFormData
+      );
+      if (data.status === 201 || 200) {
         setShowNotify(true);
-        setNotifyData({...notifyData, message:data.data.message, type: 'success'});
+        setNotifyData({
+          ...notifyData,
+          message: data.data.message,
+          type: "success",
+        });
+      } else {
+        setShowNotify(true);
+        setNotifyData({
+          ...notifyData,
+          message: "Error:Please reload your application",
+          type: "danger",
+        });
       }
     } catch {
       setShowNotify(true);
-      setNotifyData({...notifyData, message: "Error:Please reload your application", type: 'danger'});
+      setNotifyData({
+        ...notifyData,
+        message: "Error:Please reload your application",
+        type: "danger",
+      });
     }
   };
 
@@ -42,26 +61,58 @@ export default function Signup() {
     <div className="signUp w-25 p-5 border border-dark m-5">
       <Form>
         <Form.Group className="mb-3" controlId="formName">
-          <Form.Label>Enter Name</Form.Label>
-          <Form.Control name="name" type="text" placeholder="Enter name" onChange={(e) => setFormData(e)}/>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            name="name"
+            type="text"
+            onChange={(e) => setFormData(e)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control name="email" type="email" placeholder="Enter email" onChange={(e) => setFormData(e)}/>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            name="email"
+            type="email"
+            onChange={(e) => setFormData(e)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control
+            name="phone"
+            type="number"
+            onChange={(e) => setFormData(e)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" placeholder="Password" onChange={(e) => setFormData(e)}/>
+          <Form.Control
+            name="password"
+            type="password"
+            onChange={(e) => setFormData(e)}
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit" onClick={(e) => registerUser(e)}>
+        <Button
+          style={{ backgroundColor: "var(--primary-color)" }}
+          variant="primary"
+          type="submit"
+          onClick={(e) => registerUser(e)}
+        >
           Signup
         </Button>
       </Form>
       <br></br>
-      {showNotify && <Notify message={notifyData.message} type={notifyData.type} setShowNotify={setShowNotify}/>}
+      {showNotify && (
+        <Notify
+          message={notifyData.message}
+          type={notifyData.type}
+          setShowNotify={setShowNotify}
+        />
+      )}
     </div>
   );
 }
